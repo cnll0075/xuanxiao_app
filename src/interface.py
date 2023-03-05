4bf71c5baf87b23d9af17d079ef62b743083ec4d
Leiling Tao <angelatao0123@gmail.com>
2023-03-05 12:12:32 -0800

    initial commit

---
+++ src/interface.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel
+
+class Major(BaseModel):
+    schoolName: str
+    schoolEnglishName: str
+    specializedSubject: str
+    region: str
+    qsRanking: str
+    qsRanking_top_100: bool
+    major_id: str
+
+class TieredResult(BaseModel):
+    tierRanking: int
+    tierName: str
+    majorList: list[Major]
+
+tier_name_mapping = {0: '冲刺学校', 1: '安全学校', 2: '保底学校'}
+
+class InputMessage(BaseModel):
+    applicantUndergrad: str
+    tuitionRange: str
+    countryInterested: str
+    majorInterested: str
+    estimatedEntranceTime: str
+    highestDegree: str
+    GPA: float
+    grade: str
